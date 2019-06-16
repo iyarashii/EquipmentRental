83dc7ec5e00edbcd84540dfee6d6f1ce524c8ef7
Iyarashii <strider1337v@gmail.com>
2019-06-16 18:41:30 +0200

    Added comments.

--- EquipmentRental/EquipmentRental/Models/IsRentedAndEndDateToColorConverter.cs
+++ EquipmentRental/EquipmentRental/Models/IsRentedAndEndDateToColorConverter.cs
@@ -7,40 +7,57 @@ using Xamarin.Forms;
 
 namespace EquipmentRental
 {
+    // custom value converter used to convert boolean values into Color objects
+    // this class is used to change background color of the grid in listview viewcells
     class IsRentedAndEndDateToColorConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        // implementation of IValueConverter.Convert method
+        // this method receives values from 2 binded properties one as value parameter and 2nd as secondValue parameter
+        public object Convert(object value, Type targetType, object secondValue, CultureInfo culture)
         {
             DateTime endDate;
-            if (parameter is Label endDateLabel)
+            // pattern matching used to check if 2nd value is Label
+            if (secondValue is Label endDateLabel)
             {
+                // assignment of Label.Text property to local variable
                 var endDateLabelValue = endDateLabel.Text;
                 try
                 {
+                    // parase string to DateTime
                     endDate = DateTime.ParseExact(endDateLabelValue, "D", CultureInfo.CurrentCulture);
                 }
                 catch
                 {
+                    // if parsing not successful don't change color
                     return Color.Default;
                 }
             }
             else
             {
+                // if 2nd object is not Label don't change color
                 return Color.Default;
             }
+
             if ((bool)value && endDate >= DateTime.Today)
             {
+                // change color to green if rental end date is today or in the future
                 return Color.Green;
             }
             else if((bool)value && endDate < DateTime.Today)
             {
+                // change color to red if end date was before today
                 return Color.Red;
             }
+
+            // return default color if value of the 1st binding is false
+            // if IsRented is false do not change Color
             return Color.Default;
         }
 
+        // IValueConverter.ConvertBack method implementation template
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            // not implemented as it is never used
             throw new NotImplementedException();
         }
     }
--- EquipmentRental/EquipmentRental/Models/IsWaitingForPermissionToColorConverter.cs
+++ EquipmentRental/EquipmentRental/Models/IsWaitingForPermissionToColorConverter.cs
@@ -7,15 +7,22 @@ using Xamarin.Forms;
 
 namespace EquipmentRental
 {
+    // custom value converter used to convert boolean value into Color object
+    // this class is used to change background color of the grid in listview viewcells
     public class IsWaitingForPermissionToColorConverter : IValueConverter
     {
+        // implementation of IValueConverter.Convert method
+        // this method returns Color based on bool value of received property from Binding
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            // return yellow color if binding property equals true, default color if it is false
             return ((bool)value ? Color.Yellow : Color.Default);
         }
 
+        // IValueConverter.ConvertBack method implementation template
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            // not implemented as it is never used
             throw new NotImplementedException();
         }
     }
