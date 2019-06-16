2c5f5f5c7b5209f8c0b8c292914eb2454a7aa696
Iyarashii <strider1337v@gmail.com>
2019-06-16 11:46:21 +0200

    Added comments.

--- EquipmentRental/EquipmentRental/Models/ActivityIndicatorScope.cs
+++ EquipmentRental/EquipmentRental/Models/ActivityIndicatorScope.cs
@@ -6,38 +6,48 @@ using Xamarin.Forms;
 
 namespace EquipmentRental
 {
-    public class ActivityIndicatorScope : IDisposable
+    // class containing logic for ActivityIndicator
+    public sealed class ActivityIndicatorScope : IDisposable
     {
-        private bool showIndicator;
-        private ActivityIndicator indicator;
-        private Task indicatorDelay;
+        // declaration of private fields
+        private readonly bool showIndicator;
+        private readonly ActivityIndicator indicator;
+        private readonly Task indicatorDelay;
 
-        // TO DO get rid of something here
+        // public constructor
         public ActivityIndicatorScope(ActivityIndicator indicator, bool showIndicator)
         {
+            // assigning constructor parameters to local fields   
             this.indicator = indicator;
             this.showIndicator = showIndicator;
+
             if (showIndicator)
             {
+                // set indicator delay to 2 seconds and call SetIndicatorActivity method with parameter true
                 indicatorDelay = Task.Delay(2000);
                 SetIndicatorActivity(true);
             }
             else
             {
+                // set indicator delay to the completed task with result 0
                 indicatorDelay = Task.FromResult(0);
             }
         }
 
+        // method used for setting properties of ActivityIndicator visual element
         private void SetIndicatorActivity(bool isActive)
         {
+            // set IsVisible and IsRunning properties to isActive parameter
             this.indicator.IsVisible = isActive;
             this.indicator.IsRunning = isActive;
         }
 
+        // implementation of IDisposable.Dispose 
         public void Dispose()
         {
             if (showIndicator)
             {
+                // sets properties of ActivityIndicator visual element to false during disposing of ActivityIndicatorScope object
                 indicatorDelay.ContinueWith(t => SetIndicatorActivity(false), TaskScheduler.FromCurrentSynchronizationContext());
             }
         }
