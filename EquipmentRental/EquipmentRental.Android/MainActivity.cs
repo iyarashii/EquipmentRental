6955ee03353233830480ea3dfc3e6e66aae52bbd
Iyarashii <strider1337v@gmail.com>
2019-06-16 10:52:33 +0200

    Added Azure Mobile Apps init.

--- EquipmentRental/EquipmentRental.Android/MainActivity.cs
+++ EquipmentRental/EquipmentRental.Android/MainActivity.cs
@@ -7,6 +7,11 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Microsoft.WindowsAzure.MobileServices;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
 namespace EquipmentRental.Droid
 {
     [Activity(Label = "EquipmentRental", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -14,11 +19,11 @@ namespace EquipmentRental.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            //TabLayoutResource = Resource.Layout.Tabbar;
-            //ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            // Initialize Azure Mobile Apps
+            CurrentPlatform.Init();
+            Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
     }
