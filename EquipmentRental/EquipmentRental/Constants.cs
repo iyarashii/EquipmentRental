015e5cc197e527cec2b8969a9883f3e7b085ffe6
Iyarashii <strider1337v@gmail.com>
2019-07-17 22:15:00 +0200

    Changed app URL to empty string.

--- EquipmentRental/EquipmentRental/Constants.cs
+++ EquipmentRental/EquipmentRental/Constants.cs
@@ -7,7 +7,7 @@ namespace EquipmentRental
     // Utility class with constants
     public static class Constants
     {
-        // Azure Mobile App endpoint
-        public const string ApplicationURL = @"https://appntp.azurewebsites.net";
+        // set your Azure Mobile App endpoint here
+        public const string ApplicationURL = @"";
     }
 }
